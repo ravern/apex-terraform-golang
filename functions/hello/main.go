bed4be230732ef51b28d68997fdf5c472c99b28b
Ravern Koh <ravern.koh.dev@gmail.com>
2017-12-24 22:36:56 +0800

    Added json unmarshalling

--- functions/hello/main.go
+++ functions/hello/main.go
@@ -2,17 +2,57 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"os"
+	"time"
 
 	apex "github.com/apex/go-apex"
+	log "github.com/sirupsen/logrus"
 )
 
+// Event JSON
+type event struct {
+	QueryStringParameters struct {
+		Value int `json:"value"`
+	} `json:"queryStringParameters"`
+	Headers struct {
+		XForwardedFor string `json:"X-Forwarded-For"`
+	}
+}
+
+// Counter model
+type counter struct {
+	UserIP    string
+	Timestamp time.Time
+	Value     int
+}
+
 func main() {
+	log.SetOutput(os.Stderr)
 	apex.HandleFunc(handle)
 }
 
 func handle(evt json.RawMessage, ctx *apex.Context) (interface{}, error) {
+	// Unmarshal the JSON
+	var e event
+	if err := json.Unmarshal(evt, &e); err != nil {
+		return nil, errors.New("Integer 'value' is required.")
+	}
+
+	// Extract parameters
+	_ = extract(e)
+
+	// Construct response
+	return response(evt), nil
+}
+
+func extract(e event) counter {
+	return counter{}
+}
+
+func response(evt json.RawMessage) map[string]interface{} {
 	return map[string]interface{}{
 		"statusCode": 200,
-		"body":       `{ "hello": "world" }`,
-	}, nil
+		"body":       string(evt),
+	}
 }
